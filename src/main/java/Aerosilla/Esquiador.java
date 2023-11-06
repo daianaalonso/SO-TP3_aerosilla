0e4bce99e447ca8c6426c151396404c386de529b
Daiana <alonsodaiana5@gmail.com>
2023-11-05 21:07:29 -0300

    cambios finales

--- src/main/java/Aerosilla/Esquiador.java
+++ src/main/java/Aerosilla/Esquiador.java
@@ -1,15 +1,17 @@
-package opcion1;
+package Aerosilla;
 
 import java.util.concurrent.Semaphore;
 
 public class Esquiador extends Thread {
     private Semaphore barreraEsquiadores;
+    private Semaphore sillaLlena;
     private Semaphore barreraBajar;
     private int id;
 
-    public Esquiador(int id, Semaphore barreraEsquiadores, Semaphore barreraBajar) {
-        this.barreraEsquiadores = barreraEsquiadores;
+    public Esquiador(int id, Semaphore barreraEsquiadores, Semaphore sillaLlena, Semaphore barreraBajar) {
         this.id = id;
+        this.barreraEsquiadores = barreraEsquiadores;
+        this.sillaLlena = sillaLlena;
         this.barreraBajar = barreraBajar;
     }
 
@@ -20,7 +22,9 @@ public class Esquiador extends Thread {
                 barreraEsquiadores.acquire(); // LLega el esquiador y espera la silla
                 System.out.println("Esquiador " + id + " sentado");
 
-                Thread.sleep(5000);// Tiempo que tarda en subir la silla
+                sillaLlena.release(); // Avisa que se sentó en la silla
+
+                Thread.sleep(3000);
 
                 System.out.println("Esquiador " + id + " bajando");
                 barreraBajar.release(); // Avisa que se bajó de la silla
--- src/main/java/Aerosilla/Silla.java
+++ src/main/java/Aerosilla/Silla.java
@@ -1,4 +1,4 @@
-package opcion2;
+package Aerosilla;
 
 import java.util.concurrent.Semaphore;
 
@@ -7,8 +7,10 @@ public class Silla extends Thread {
     private Semaphore barreraBajar;
     private Semaphore mutex;
     private Semaphore sillaLlena;
+    private int id;
 
-    public Silla(Semaphore barreraEsquiadores, Semaphore mutex, Semaphore sillaLlena, Semaphore barreraBajar) {
+    public Silla(int id, Semaphore barreraEsquiadores, Semaphore mutex, Semaphore sillaLlena, Semaphore barreraBajar) {
+        this.id = id;
         this.barreraEsquiadores = barreraEsquiadores;
         this.mutex = mutex;
         this.sillaLlena = sillaLlena;
@@ -18,21 +20,24 @@ public class Silla extends Thread {
     public void run() {
         while (true) {
             try {
+                System.out.println("Silla " + id + " esperando");
+
                 mutex.acquire();//unica silla disponible
-                System.out.println("Silla disponible. Esperando esquiadores...");
+                System.out.println("Silla " + id + " disponible. Esperando esquiadores");
 
                 barreraEsquiadores.release(4); //le avisa a los esquiadores
 
                 sillaLlena.acquire(4); //espera que se suban los esquiadores
-                System.out.println("Subiendo a la cima...");
+
+                System.out.println("Subiendo a la cima");
 
                 barreraBajar.acquire(4); //espera que se bajen los esquiadores
 
-                System.out.println("Silla volviendo a la base...");
+                System.out.println("Silla " + id + " volviendo a la base");
                 mutex.release();//libero para que otra silla este disponible
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-       }
+        }
     }
 }
